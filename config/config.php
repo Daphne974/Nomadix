af052da1215e1e4f5abaa87a2d93debcc9c85207
Daphne974 <daphneliret@gmail.com>
2026-04-16 06:56:06 +0200

    Structure MVC en cours...

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+// config/config.php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'voyage');
+
+function normalizeString($str) {
+    if (function_exists('mb_check_encoding') && function_exists('mb_convert_encoding') && !mb_check_encoding($str, 'UTF-8')) {
+        $str = mb_convert_encoding($str, 'UTF-8', 'ISO-8859-1, Windows-1252');
+    }
+    if (function_exists('mb_strtolower')) {
+        $str = mb_strtolower($str, 'UTF-8');
+    } else {
+        $str = strtolower($str);
+    }
+    if (function_exists('transliterator_transliterate')) {
+        $str = transliterator_transliterate('Any-Latin; Latin-ASCII;', $str);
+    } else {
+        $converted = @iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $str);
+        if ($converted !== false) $str = $converted;
+    }
+    $str = preg_replace('/[^a-z0-9]+/', '', $str);
+    return $str ?? '';
+}
